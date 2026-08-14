5192f4663573aeee9429eb93da2aa1d2e05162af
SabyaOffice <sachizdash@gmail.com>
2026-08-14 18:51:53 +0530

    config done from office lap

--- src/oops/Demo.java
+++ src/oops/Demo.java
@@ -1,5 +1,15 @@
 package oops;
 
 public class Demo {
-
+	public static void main(String[] args) {
+		
+		Student st1;
+		st1 = new Student();
+		st1.studentName = "Sabyasachi";
+		st1.studentId = 31;
+		st1.studentCity = "Bangalore";
+		
+		st1.study();
+		st1.showFullDetails();
+	}
 }
--- src/oops/Student.java
+++ src/oops/Student.java
@@ -8,14 +8,13 @@ public class Student {
 	
 	//Behaviour : member methods : methods: (functions)
 	public void study() {
-		System.out.println(studentName + "is studying");
+		System.out.println(studentName + " is studying.");
 	}
 	
 	public void showFullDetails() {
 		System.out.println("My name is "+ studentName);
 		System.out.println("My id is "+ studentId);
 		System.out.println("My city is "+ studentCity);
-
 		}
-
+	
 }
