664dbbc5fe0a76f624984e3fb3aeed1ef87e7262
SabyaOffice <dashsabyasachi32@gmail.com>
2026-08-14 17:36:17 +0530

    config dne

--- src/Pack1/LoopsEx.java
+++ src/Pack1/LoopsEx.java
@@ -3,11 +3,11 @@ package Pack1;
 public class LoopsEx {
 	public static void main(String[] args) {
 		//For loop
-		for(int i =1;i<=10;i++) 
-			if(i%2 == 0) {
-				System.out.println(i);
-				System.out.println(i+1);
-			}
+//		for(int i =1;i<=10;i++) 
+//			if(i%2 == 0) {
+//				System.out.println(i);
+//				System.out.println(i+1);
+//			}
 		//While Loop
 //		int i = 1;
 //		while( i <= 10) {
@@ -27,7 +27,14 @@ public class LoopsEx {
 //		for (int num:numbers) {
 //			System.out.println(num);
 //			}
-		}	
+		
+//		char [] letr = {'a','b','c','d','e'};
+//		for (char l:letr) {
+//			System.out.println(l);
+//		}
+	 	
+		
+		
 	}
 
-
+}
