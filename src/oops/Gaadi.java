d424fa4cf53b1e87bc8f66c4ad8ba0834d204449
Sabya <sdash0113@gmail.com>
2026-08-17 23:14:25 +0530

    Updated Gaadi class

--- src/oops/Gaadi.java
+++ src/oops/Gaadi.java
@@ -4,9 +4,9 @@ public class Gaadi {
 
     public static void main(String[] args) {
 
-        FourWheelers car1, car2;
+        FourWheelers car1;
         car1 = new FourWheelers();
-        car2 =new FourWheelers(); 
+         
 
         car1.carName = "Toyota.";
         car1.carNumber = "OD02AB1234.";
@@ -15,13 +15,13 @@ public class Gaadi {
         car1.carDetails();
         car1.name();
         
-        
-        
+         
+        FourWheelers car2 = new FourWheelers();
         car2.carName = "Skoda.";
         car2.seatSize = 4;
         car2.carNumber = "KA01A3412.";
         
         car2.name();
-        car2.carDetails();
+        car2.carDetails(); 
     }
 }
