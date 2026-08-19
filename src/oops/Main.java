91b1eca484c10a71b241fbc8748c6b39b1ebaf3b
Sabya <sdash0113@gmail.com>
2026-08-19 11:20:00 +0530

    config done ad deleting the encap example

--- src/oops/Main.java
+++ src/oops/Main.java
@@ -5,17 +5,6 @@ public class Main {
 		Son son = new Son();
 		son.dadProperty();
 		
-		
-		
-		Mobile mobile = new Mobile();
-
-        mobile.setBrand("Samsung");
-        mobile.setModel("Galaxy S25");
-        mobile.setPrice(80000);
-
-        System.out.println(mobile.getBrand());
-        System.out.println(mobile.getModel());
-        System.out.println(mobile.getPrice());
     }
 }
 
