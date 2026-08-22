93143ce2a73fe196762bfb19223be127eb581313
Sabya <sdash0113@gmail.com>
2026-08-23 00:01:47 +0530

    practiced all least basic programs

--- src/basic_Prog/Number_programs.java
+++ src/basic_Prog/Number_programs.java
@@ -2,31 +2,36 @@ package basic_Prog;
 
 public class Number_programs {
 	public static void main(String[] args) {
-		int num, temp, total, count;
 		
-		num = 153;
-		temp = num;
-		total = 0;
-		count = 0;
+////		Armstrong Number
+//		int num, temp, total, count;
+//		
+//		num = 153;
+//		temp = num;
+//		total = 0;
+//		count = 0;
+//		
+//		while (temp > 0) {
+//			count += 1;
+//			temp /= 10;
+//		}
+//			
+//		temp = num;
+//		
+//		while (temp>0) {
+//			int rem = temp % 10;
+//			total += Math.pow(rem,count);
+//			temp /= 10;
+//		}
+//		
+//		if(total == num) {
+//			 System.out.println("Number is Armstrong ");
+//		} else {
+//			System.out.println("Number is not Armstrong ");		
+//		}
 		
-		while (temp > 0) {
-			count += 1;
-			temp /= 10;
-		}
-			
-		temp = num;
 		
-		while (temp>0) {
-			int rem = temp % 10;
-			total += Math.pow(rem,count);
-			temp /= 10;
-		}
 		
-		if(total == num) {
-			 System.out.println("Number is Armstrong ");
-		} else {
-			System.out.println("Number is not Armstrong ");		
-		}
 		
 	}
 }
---
+++ src/basic_Prog/PracticeProg.java
@@ -0,0 +1,66 @@
+package basic_Prog;
+
+public class PracticeProg {
+
+	public static void main(String[] args) {
+		
+//		Hello World	
+//		System.out.println("Hello World");
+		
+//		Add two number
+//		int a = 45;
+//		int b = 25;
+//		System.out.println(a+b);
+		
+//		Subtract two numbers
+//		int a = 45;
+//		int b = 25;
+//		System.out.println(a-b);
+		
+//		Multiply two numbers
+//		int a = 45;
+//		int b = 5;
+//		System.out.println(a*b);
+		
+//		Even/odd
+//		int a = 40;
+//		if (a%2 == 0) {
+//			System.out.println(a + " is Even");
+//		}else {
+//			System.out.println(a + " is Odd");
+//		}	
+		
+//		Positive/negative
+//		int a = -40;
+//		if (a>0) {
+//			System.out.println(a + " is a positive number");
+//		}else {
+//			System.out.println(a + " is a negative number");
+//		}
+		
+//		Largest of two numbers
+//		int a = 100;
+//		int b = 40;
+//		if (a > b) {
+//			System.out.println(a);
+//		}else {
+//			System.out.println(b);
+//		}
+		
+//		Largest of three numbers
+		int a = 100;
+		int b = 400;
+		int c = 1252; 
+		int largest;
+		
+		if (a >= b && a >= c) {
+			largest = a;
+		}else if (b >= a && b >= c){
+			largest = b;
+		}
+		else {
+			largest = c;
+		}
+		System.out.println("Largest number: " + largest);
+	}
+}
