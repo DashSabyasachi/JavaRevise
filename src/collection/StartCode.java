18e2bc22c580f6936663029768927213a25d73e2
SabyaOffice <sachizdash@gmail.com>
2026-08-25 17:42:54 +0530

    confog done

--- src/collection/StartCode.java
+++ src/collection/StartCode.java
@@ -1,7 +1,16 @@
 package collection;
-
+import java.util.*;
 public class StartCode {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java code collections.");
+		ArrayList<String> names = new ArrayList<>();
+		names.add("Sabyasachi");
+		names.add("Ramesh");
+		names.add("Sonu");
+		System.out.println(names);
+		
+		LinkedList lst = new LinkedList();
+		
+		
 	}
 }
