81f43f68f5eca045fa06f31eceb6e252d2c52f97
SabyaOffice <sachizdash@gmail.com>
2026-08-26 18:52:30 +0530

    config done

--- src/collection/StartCode.java
+++ src/collection/StartCode.java
@@ -1,15 +1,22 @@
 package collection;
 import java.util.*;
 public class StartCode {
+	   
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java code collections.");
 		ArrayList<String> names = new ArrayList<>();
 		names.add("Sabyasachi");
 		names.add("Ramesh");
-		names.add("Sonu");
+		names.add("Sonu");  
 		System.out.println(names);
+		System.out.println(names.get(0));
+		System.out.println(names.get(2));
 		
 		LinkedList lst = new LinkedList();
+		lst.add("E-City");
+		lst.add("Blr");
+		lst.addAll(Arrays.asList("Dhl","Hyd"));
+		System.out.println(lst);
 		
 	}
-}
+} 
