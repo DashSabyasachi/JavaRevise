a71e388bbe6b61aa91a706771a94b2f9c19db619
Sabya <sdash0113@gmail.com>
2026-08-31 14:28:40 +0530

    config done

--- src/basic_Prog/Number_programs.java
+++ src/basic_Prog/Number_programs.java
@@ -47,16 +47,16 @@ public class Number_programs {
 		
 		
 //		Fibonacci series
-//		public static void main(String[] args) {
-//			int a = 0, b = 1;
-//			for(int i = 1;i <=10; i ++) {
-//				System.out.println(a);
-//				int c = a+b;
-//				a = b;
-//				b = c;		
-//			}
-//			
-//		}	
+		public static void main(String[] args) {
+			int a = 0, b = 1;
+			for(int i = 1;i <=10; i ++) {
+				System.out.println(a);
+				int c = a+b;
+				a = b;
+				b = c;		
+			}
+			
+		}	
 	
 	
 //		Reverse a number
@@ -122,18 +122,18 @@ public class Number_programs {
 	
 	
 //		Prime number
-		public static void main(String[] args) {
-			int num = 7;
-			int count = 0;
-			for(int i =1;i <=num;i++) {
-				if(num%i==0) {
-					count+=1;
-				}
-			}
-			if(num != 1 && count ==2) {
-				System.out.println("IsPrime");
-			}else {
-				System.out.println("Not Prime");
-			}
-		}
+//		public static void main(String[] args) {
+//			int num = 7;
+//			int count = 0;
+//			for(int i =1;i <=num;i++) {
+//				if(num%i==0) {
+//					count+=1;
+//				}
+//			}
+//			if(num != 1 && count ==2) {
+//				System.out.println("IsPrime");
+//			}else {
+//				System.out.println("Not Prime");
+//			}
+//		}
 }
