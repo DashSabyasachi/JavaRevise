5706975f366a0138f9c6e0bffd912ef7811374f5
Sabya <sdash0113@gmail.com>
2026-09-01 10:48:35 +0530

    config done

--- src/Pack1/LoopsEx.java
+++ src/Pack1/LoopsEx.java
@@ -3,30 +3,30 @@ package Pack1;
 public class LoopsEx {
 	public static void main(String[] args) {
 		//For loop
-//		for(int i =1;i<=10;i++) 
-//			if(i%2 == 0) {
-//				System.out.println(i);
-//				System.out.println(i+1);
-//			}
+		for(int i =1;i<=10;i++) 
+			if(i%2 == 0) {
+				System.out.println(i);
+				System.out.println(i+1);
+			}
 		//While Loop
-//		int i = 1;
-//		while( i <= 10) {
-//			System.out.println(i);
-//		i++;
-		
+		int i = 1;
+		while( i <= 10) {
+			System.out.println(i);
+			i++;
+			}
 		//do while loop
-//		int i = 10;
-//
-//        do {
-//            System.out.println(i);
-//            i++;
-//        } while (i <= 15);
+		i = 10;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 15);
  		
 		// for each
-//		int[] numbers = {10,20,30,40,50};
-//		for (int num:numbers) {
-//			System.out.println(num);
-//			}
+		int[] numbers = {10,20,30,40,50};
+		for (int num:numbers) {
+			System.out.println(num);
+			}
 		}	
 	}
 
