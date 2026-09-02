2e9f276c74dfcffda0fc7b0e8bf1d2189433b6ce
Sabya <sdash0113@gmail.com>
2026-09-02 10:02:58 +0530

    config done

---
+++ src/Pack1/Practice.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+public class Practice {
+public static void main(String[] args) {
+	int num ;
+	num = 5;
+	int num2;
+	num2 = 5;
+	String str1 = "JAVA";
+	String str2 = "JAVA";
+	System.out.println(str1 == str2);
+	System.out.println(str1.equals(str2));
+	
+	String s = "Hello";
+	s = "Java";
+	System.out.println(s);
+}
+}
