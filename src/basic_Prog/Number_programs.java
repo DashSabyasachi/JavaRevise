c3e0bff397934b16c2c6744b14d67b3330fa2f17
Sabya <sdash0113@gmail.com>
2026-09-03 22:11:42 +0530

    config done

--- src/basic_Prog/Number_programs.java
+++ src/basic_Prog/Number_programs.java
@@ -4,31 +4,31 @@ public class Number_programs {
 	
 	
 //Armstrong Number
-	public static void main(String[] args) {	
-		
-		int num, temp, total, count;	
-		num = 153;
-		temp = num;
-		total = 0;
-		count = 0;
-		while (temp > 0) {
-			count += 1;
-			temp /= 10;
-		}
-		
-		temp = num;	
-		
-		while (temp>0) {
-			int rem = temp % 10;
-			total += Math.pow(rem,count);
-			temp /= 10;
-		}	
-		if(total == num) {
-			 System.out.println("Number is Armstrong ");
-		} else {
-			System.out.println("Number is not Armstrong ");		
-		}
-}	
+//	public static void main(String[] args) {	
+//		
+//		int num, temp, total, count;	
+//		num = 153;
+//		temp = num;
+//		total = 0;
+//		count = 0;
+//		while (temp > 0) {
+//			count += 1;
+//			temp /= 10;
+//		}
+//		
+//		temp = num;	
+//		
+//		while (temp>0) {
+//			int rem = temp % 10;
+//			total += Math.pow(rem,count);
+//			temp /= 10;
+//		}	
+//		if(total == num) {
+//			 System.out.println("Number is Armstrong ");
+//		} else {
+//			System.out.println("Number is not Armstrong ");		
+//		}
+//}	
 		
 	
 //		Factorial of a number
