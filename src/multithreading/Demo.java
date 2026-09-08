f19359b268449b7adaad9719b71332d6f515bffd
Sabya <sdash0113@gmail.com>
2026-09-08 23:06:07 +0530

    practicing multi threading

--- src/multithreading/Demo.java
+++ src/multithreading/Demo.java
@@ -1,21 +1,96 @@
 package multithreading;
 
+// // Single thread
+//class MyThread extends Thread {
+//
+//    public void run() {
+//        System.out.println("My thread is running");
+//    }
+//}
+//
+//
+//public class Demo {
+//
+//    public static void main(String[] args) {
+//
+//        MyThread t1 = new MyThread();
+//
+//        t1.start();
+//    }
+//}
 
-class MyThread extends Thread {
+
+
+
+// // Multiple thread
+//class MyThread extends Thread {
+//
+//    public void run() {
+//
+//        for(int i = 1; i <= 5; i++) {
+//            System.out.println(Thread.currentThread().getName()
+//                    + " : " + i);
+//        }
+//    }
+//}
+//
+//public class Demo {
+//
+//    public static void main(String[] args) {
+//
+//        MyThread t1 = new MyThread();
+//        MyThread t2 = new MyThread();
+//
+//        t1.start();
+//        t2.start();
+//    }
+//}
+
+
+// //Runnable
+class MyTask implements Runnable {
 
     public void run() {
-        System.out.println("My thread is running");
+        System.out.println("Task is running");
     }
 }
-
 
 public class Demo {
 
     public static void main(String[] args) {
 
-        MyThread t1 = new MyThread();
+        MyTask task = new MyTask();
+
+        Thread t1 = new Thread(task);
 
         t1.start();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
