2b0dc2adb52364807be0b7998f29cf4ce29f186d
Sabya <sdash0113@gmail.com>
2026-09-12 23:37:03 +0530

    config done

--- src/stringg/Stringg1.java
+++ src/stringg/Stringg1.java
@@ -46,8 +46,10 @@ public static void main(String[] args) {
 	
 	String a = new String("Hello");
 	String b = new String("Hello");
+	String c = new String("Hello");
 
 	System.out.println(a == b);
 	System.out.println(a.equals(b));
+	System.out.println(a.equals(c));
 }
 }
